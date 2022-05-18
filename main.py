cb2af985b9a2d7a9c12504563bbd0beea4c5a12a
Evgeniy Geraskin <zgeraskin@gmail.com>
2022-05-18 14:27:57 +0300

    Version_0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+#импорты
+
+
+
+#глобальные перменные
+FIELD = [[' ']*3 for _ in range(3)]
+
+#функции
+def field():
+    global FIELD
+    pass
+
+
+#чтение .ini файла
+
+
+
+#запуск суперцикла
+while True:
+    command = input()
+
+
+    if command in ('quit','выход')
+        break
+
+    #ввод имени игрока
+
